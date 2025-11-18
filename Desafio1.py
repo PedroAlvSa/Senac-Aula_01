563707feb5d876233ebd52753ab62277516b131b
F4NG1 <pedrosantosf3@gmail.com>
2025-11-18 19:42:22 -0300

    Inicia o repositório da Aula 01

---
+++ Desafio1.py
@@ -0,0 +1,3 @@
+vendas=13000; meta=15000
+cal=(((meta-vendas)/100)-100)*-1
+print(f'{cal:.2f}%')
---
+++ Exemplo.py
@@ -0,0 +1,60 @@
+# nome='Ana'
+# idade=25
+# salario=15000.0
+# print(f'O nome dela é: {nome}, \nidade {idade} \ne o salario de {salario}')
+
+# x=5
+# x='10'
+# print(x)
+
+# nome=input('Digite o nome: ')
+# print(f'{nome}')
+
+# cor=input('Qual a sua cor favorita? ')
+# print(f'Sua cor favorita é: {cor}')
+
+# n1=5
+# n2=3
+# resultado=n1+n2
+# print(f'O resultado foi {resultado}')
+
+# n1=int(input('Digite o primeiro número: '))
+# n2=int(input('Digite o segundo número: '))
+# resultado=n1*n2
+# print(f'O resultado é: {resultado}')
+
+# nome_usuario=input('Informe o nome: ')
+# idade_usuario=input('Informe a idade: ')
+# print(f'Seu Nome é: {nome_usuario} e sua idade é: {idade_usuario}')
+
+# cidade=input('Informe sua cidade: ')
+# bairro=input('Informe o seu bairro: ')
+# barricada=input('Possui barricadas? S/N ')
+# print(f'Sua cidade é: {cidade}, seu bairro é: {bairro} ')
+# if barricada== 'S':
+#     print('Seu bairro tem barricada')
+# else:
+#     print('Seu bairro não tem barricada')
+
+# idade=int(input('Informe sua idade: '))
+# i1=idade; i2=2025; ano=i2-i1
+# print(f'O ano que você nasceu é: {ano}')
+
+# va=float(input('Qual o valor que deseja usar para comprar os ingresso? '))
+# ing=45
+# va1=va//ing
+# print(f'Você consegue comprar {va1 :.0f} ingressos')
+
+# preco=float(input(f'Informe o preco: '))
+# qtd=int(input('Informe a quantidade: '))
+# total=preco*qtd; desconto=total*0.1; valor_final=total-desconto
+# print(f'Valor total {total:.2f}\nDesconto de 10% {desconto :.2f}\nValor a pagar: {valor_final:.2f}')
+
+
+
+sal=float (input('Qual o seu salário? '))
+sal1=sal+(sal*0.15)
+print(f'Seu novo salário é de: {sal1:.2f}\nAcréscimo de: {sal*0.15:.2f}')
+
+
+
